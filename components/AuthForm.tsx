ba4489c9b8922534584e3a8170290e782aa7b73a
Samuel <109113326+samuxka@users.noreply.github.com>
2025-03-29 17:42:40 -0300

    implement sign-in, sign-up and auth

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,19 +1,21 @@
 "use client"
 
-import {zodResolver} from "@hookform/resolvers/zod";
-import {useForm} from "react-hook-form"
-import {z} from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form"
+import { z } from "zod";
 
 // Shadcn import components
-import {Button} from "@/components/ui/button"
-import {Form} from "@/components/ui/form"
+import { Button } from "@/components/ui/button"
+import { Form } from "@/components/ui/form"
 import Image from "next/image";
 import Link from "next/link";
-import {toast} from "sonner";
+import { toast } from "sonner";
 import FormField from "@/components/FormField";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 
-import { Icon } from "@iconify/react";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/firebase/client";
+import { signIn, signUp } from "@/lib/actions/auth.actions";
 
 const authFormSchema = (type: FormType) => {
     return z.object({
@@ -23,7 +25,7 @@ const authFormSchema = (type: FormType) => {
     })
 }
 
-const AuthForm = ({type}: {type: FormType}) => {
+const AuthForm = ({ type }: { type: FormType }) => {
     const router = useRouter();
     const formSchema = authFormSchema(type)
     // 1. Define my form
@@ -37,16 +39,43 @@ const AuthForm = ({type}: {type: FormType}) => {
     })
 
     // 2. Define a submit handler
-    function onSubmit(values:z.infer<typeof formSchema>){
-        try{
-            if(type === "sign-up"){
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+        try {
+            if (type === "sign-up") {
+                const { name, email, password } = values
+                const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+                const result = await signUp({
+                    uid: userCredentials.user.uid,
+                    name: name!,
+                    email,
+                    password,
+                })
+
+                if (!result?.success) {
+                    toast.error(result?.message)
+                    return
+                }
+
                 toast.success("Account created successfully. &#129395;")
                 router.push('/sign-in')
-            }else{
+            } else {
+                const { email, password } = values
+                const userCredentials = await signInWithEmailAndPassword(auth, email, password)
+                const idToken = await userCredentials.user.getIdToken()
+
+                if (!idToken) {
+                    toast.error('Sign in failed')
+                    return
+                }
+
+                await signIn({
+                    email, idToken
+                })
+
                 toast.success("Sign in successfully.")
                 router.push('/')
             }
-        }catch (error){
+        } catch (error) {
             console.log(error)
             toast.error(`There was an error: ${error}`)
         }
@@ -57,39 +86,42 @@ const AuthForm = ({type}: {type: FormType}) => {
     return (
         <div className="card-border lg:min-w-[566px]">
             <div className="flex flex-col gap-6 card py-14 px-10 items-center justify-center">
-                <div className="flex flex-row gap-2 justify-center">
-                    <Image src="/logo.svg" alt="logo" height={32} width={32}/>
-                    <h2 className="text-primary-100">Interview-app</h2>
+                <div>
+                    <Link href="/" className="flex flex-row gap-2 justify-center">
+                        <Image src="/logo.svg" alt="logo" height={32} width={32} />
+                        <h2 className="text-primary-100">Interview-app</h2>
+                    </Link>
                 </div>
                 <h3>Pratice job interview with A.I</h3>
 
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
-                    {!isSignIn && (
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+                        {!isSignIn && (
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                label="name"
+                                placeholder="Enter your name" />
+                        )}
+
                         <FormField
                             control={form.control}
-                            name="name"
-                            label="name"
-                            placeholder="Enter your name"/>
-                    )}
+                            name="email"
+                            label="Email"
+                            placeholder={!isSignIn ? "Ex.: example@example.com" : "Enter your email"}
+                            type="email" />
 
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        label="Email"
-                        placeholder={!isSignIn ? "Ex.: example@example.com" : "Enter your email"}
-                        type="email"/>
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            label="Password"
+                            placeholder={!isSignIn ? "Create your password" : "Enter your password"}
+                            type="password"
+                        />
 
-                    <FormField
-                        control={form.control}
-                        name="password"
-                        label="Password"
-                        placeholder={!isSignIn ? "Create your password" : "Enter your password"}
-                        type="password"/>
-
-                    <Button type="submit" className="rounded-sm btn">{isSignIn ? 'Sign in': 'Create an account'}</Button>
-                </form>
-            </Form>
+                        <Button type="submit" className="rounded-sm btn">{isSignIn ? 'Sign in' : 'Create an account'}</Button>
+                    </form>
+                </Form>
                 <p className="text-center">
                     {isSignIn ? 'No account yet?' : 'Have an account already?'}
                     <Link href={!isSignIn ? '/sign-in' : '/sign-up'} className="font-bold text-user-primary ml-1">{!isSignIn ? 'Login' : 'Create an account'}</Link>
--- firebase/admin.ts
+++ firebase/admin.ts
@@ -2,9 +2,9 @@ import { initializeApp, getApps, cert } from "firebase-admin/app"
 import { getAuth } from "firebase-admin/auth"
 import { getFirestore } from "firebase-admin/firestore"
 
-const project_id = process.env.FIREBASE_PROJECT_ID
-const client_email = process.env.FIREBASE_CLIENT_EMAIL
-const private_key = process.env.FIREBASE_PRIVATE_KEY
+// const project_id = process.env.FIREBASE_PROJECT_ID
+// const client_email = process.env.FIREBASE_CLIENT_EMAIL
+// const private_key = process.env.FIREBASE_PRIVATE_KEY
  
 const initFirebaseAdmin = () => {
     const apps = getApps()
@@ -12,9 +12,9 @@ const initFirebaseAdmin = () => {
     if (!apps.length) {
         initializeApp({
             credential: cert({
-                projectId: project_id,
-                clientEmail: client_email,
-                privateKey: private_key?.replace(/\\n/g, "\n")
+                projectId: process.env.FIREBASE_PROJECT_ID,
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+                privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
 
             })
         })
