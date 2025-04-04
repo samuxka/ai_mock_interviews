2fb638abf907aace575063b30c29e6999f657ff8
Samuel <109113326+samuxka@users.noreply.github.com>
2025-04-04 08:27:06 -0300

    i finally complete this project

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -6,6 +6,7 @@ import { cn } from '@/lib/utils'
 import { useRouter } from 'next/navigation'
 import { vapi } from '@/lib/vapi.sdk'
 import { interviewer } from '@/constants'
+import { createFeedback } from '@/lib/actions/general.action'
 
 enum CallStatus {
     INACTIVE = 'INACTIVE',
@@ -61,14 +62,14 @@ const Agent = ({ userName, userId, type, interviewId, questions }: AgentProps) =
     const handleGenerateFeedback = async (messages: SavedMessage[]) => {
         console.log('generate feedback here')
 
-        const {success, id} = {
-            success: true,
-            id: 'feedback-id'
-        }
+        const {success, feedbackId: id} = await createFeedback({
+            interviewId: interviewId!,
+            userId: userId!,
+            transcript: messages
+        })
 
         if(success && id) {
             router.push(`/interview/${interviewId}/feedback`)
-            // router.push(`/interview/${id}/feedback`)
         }else{
             console.error("Error saving feedback")
             router.push('/')
--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -6,9 +6,10 @@ import { Button } from './ui/button'
 import Link from 'next/link'
 import DisplayTechIcons from './DisplayTechIcons'
 import { Calendar, Star } from 'lucide-react'
+import { getFeedbackByInterviewId } from '@/lib/actions/general.action'
 
-const InterviewCard = ({ id, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
-    const feedback = null as Feedback | null
+const InterviewCard = async ({ id, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
+    const feedback = userId && id ? await getFeedbackByInterviewId({interviewId: id, userId}): null
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY')
 
